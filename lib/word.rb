82cd78f84085e098fe4cbaf77af4e2eabc657fa4
Ellerden <ellerden@gmail.com>
2017-03-23 13:55:33 +0300

    Update word.rb

--- lib/word.rb
+++ lib/word.rb
@@ -2,9 +2,7 @@
 class Word
   def read_from_file(file_name)
     return nil if !File.exist?(file_name)
-    file = File.new(file_name, "r:UTF-8")
-    lines = file.readlines
-    file.close
+    lines = File.readlines(file_name, "r:UTF-8")
     Unicode::downcase(lines.sample.chomp)
   end
 end
